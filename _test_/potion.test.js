117571406d31d85c45120f52987cabc3698b536f
Racer <reaganjoseph26@yahoo.com>
2020-12-24 16:47:10 -0500

    potion contrcutor refractor

--- _test_/potion.test.js
+++ _test_/potion.test.js
@@ -4,3 +4,11 @@ test('creates a health potion object', ()  => {
     const potion = new Potion('health')
     expect(potion.value).toEqual(expect.any(Number))
 })
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
